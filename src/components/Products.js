53e62264ef53a3c616ac40251bd483403ddf4df5
Tyler Kirumba <Tyler>
2023-05-05 01:40:55 +0300

    final

--- src/components/Products.js
+++ src/components/Products.js
@@ -5,13 +5,10 @@ import ProductsForm from './ProductsForm';
 function Products() {
   const [products, setProducts] = useState([]);
   const [selectedProduct, setSelectedProduct] = useState(null);
-
   const [editedProduct, setEditedProduct] = useState(null);
-
   const [isFormVisible, setIsFormVisible] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [productsPerPage, setProductsPerPage] = useState(10);
-
 
   useEffect(() => {
     fetch('https://dummyjson.com/products')
@@ -25,6 +22,36 @@ function Products() {
     setSelectedProduct(product);
     setEditedProduct(null);
   };
+
+   const handleAddProductClick = () => {
+    setIsFormVisible(true);
+  };
+
+  const handleProductFormClose = () => {
+    setIsFormVisible(false);
+  };
+
+  const handleProductFormSubmit = (newProduct) => {
+    fetch('https://dummyjson.com/products/add', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(newProduct)
+    })
+      .then(res => res.json())
+      .then(data => {
+        setProducts([...products, data]);
+        setIsFormVisible(false);
+      });
+  };
+
+  // Get current products
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
+
+  // Change page
+  const paginate = pageNumber => setCurrentPage(pageNumber);
+
 
   const handleDeleteClick = (id) => {
     fetch(`https://dummyjson.com/products/${id}`, {
@@ -42,6 +69,8 @@ function Products() {
     setSelectedProduct(null);
     setEditedProduct(product);
   };
+  
+  
 
   const handleUpdateClick = () => {
     fetch(`https://dummyjson.com/products/${editedProduct.id}`, {
@@ -73,55 +102,13 @@ function Products() {
     setEditedProduct(null);
   };
 
-
-  const handleAddProductClick = () => {
-    setIsFormVisible(true);
-  };
-
-  const handleProductFormClose = () => {
-    setIsFormVisible(false);
-  };
-
-  const handleProductFormSubmit = (newProduct) => {
-    fetch('https://dummyjson.com/products/add', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(newProduct)
-    })
-      .then(res => res.json())
-      .then(data => {
-        setProducts([...products, data]);
-        setIsFormVisible(false);
-      });
-  };
-
-  // Get current products
-  const indexOfLastProduct = currentPage * productsPerPage;
-  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
-  const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
-
-  // Change page
-  const paginate = pageNumber => setCurrentPage(pageNumber);
-
-  //DELET
-
-  const handleDeleteClick = (id) => {
-    fetch(`https://dummyjson.com/products/${id}`, {
-      method: 'DELETE'
-    })
-    .then(response => response.json())
-    .then(data => {
-      // Remove the deleted product from the state
-      setProducts(products.filter(product => product.id !== id));
-    })
-    .catch(error => console.log(error));
-  };
-
-
   return (
     <div className='container-fluid'>
       <h1>Products</h1>
-
+      <button className="btn btn-primary mb-3" onClick={handleAddProductClick}>Add Product</button>
+      {isFormVisible &&
+        <ProductsForm onClose={handleProductFormClose} onSubmit={handleProductFormSubmit} />
+      }
       {selectedProduct || editedProduct ?
         <ProductDetails 
           product={editedProduct || selectedProduct}
@@ -130,14 +117,6 @@ function Products() {
           onUpdateClick={handleUpdateClick}
           onCancelClick={handleCancelClick}
         />
-
-      <button className="btn btn-primary mb-3" onClick={handleAddProductClick}>Add Product</button>
-      {isFormVisible &&
-        <ProductsForm onClose={handleProductFormClose} onSubmit={handleProductFormSubmit} />
-      }
-      {selectedProduct ?
-        <ProductDetails product={selectedProduct} />
-
         :
         <table className="table table-striped table-bordered table-md">
           <thead>
@@ -151,12 +130,7 @@ function Products() {
             </tr>
           </thead>
 
-
-            {products.map((product) => (
-
-          <tbody>
             {currentProducts.map((product) => (
-
               <tr key={product.id}>
                 <td>{product.id}</td>
                 <td>
@@ -168,11 +142,7 @@ function Products() {
                 <td>${product.price}</td>
                 <td><img src={product.thumbnail} alt={product.title} width="100" /></td>
                 <td>
-
                   <button className="btn btn-primary mr-2" onClick={() => handleEditClick(product)}>Edit</button>
-
-                  <button className="btn btn-primary mr-2">Edit</button>
-
                   <button className="btn btn-danger" onClick={() => handleDeleteClick(product.id)}>Delete</button>
                 </td>
               </tr>
@@ -193,4 +163,5 @@ function Products() {
     </div>
   );
 }
+
 export default Products;
